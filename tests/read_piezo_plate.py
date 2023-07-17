833e60edecc62b5eb8e640a7fda8f26aff412d14
JosephGakah <joegakah@gmail.com>
2023-07-17 18:17:11 +0300

    led blink once the piezo plat is tapped

--- tests/read_piezo_plate.py
+++ tests/read_piezo_plate.py
@@ -1,5 +1,5 @@
 import time
-from pyfirmata import Arduino
+from pyfirmata import Arduino, INPUT, util
 from playsound import playsound
 
 # Define the port where Arduino is connected
@@ -8,9 +8,15 @@ port = '/dev/ttyACM0'
 # Establish a connection with the Arduino
 board = Arduino(port)
 print("Communication Successfully started")
+it = util.Iterator(board)  
+it.start()  
 
 # Set up the pin modes
 led_pin = board.digital[9]
+piezo_plate_input = board.get_pin('a:3:i') 
+
+piezo_plate_input.mode = INPUT
+piezo_plate_input.enable_reporting()
 
 #Play a sound function
 def punch():
@@ -19,11 +25,10 @@ def punch():
 
 # Blink the LED
 while True:
-    led_pin.write(1)  # Turn the LED on
-    board.pass_time(1) # Delay for 1 second
-    print("LED ON...")
-    #punch()
+    print(piezo_plate_input.read())
+    time.sleep(0.1)
+    if(piezo_plate_input.read() > 0.0):
+        led_pin.write(1)
+        time.sleep(0.4)
+        led_pin.write(0)
 
-    led_pin.write(0)  # Turn the LED off
-    board.pass_time(1) # Delay for 1 second
-    print("LED OFF...")
