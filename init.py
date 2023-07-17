66f67f0fa05e8cfd97e3b8d00551bf83d11cfd24
JosephGakah <joegakah@gmail.com>
2023-07-17 13:48:10 +0300

    blink arduino python

---
+++ init.py
@@ -0,0 +1,22 @@
+import time
+from pyfirmata import Arduino
+
+# Define the port where Arduino is connected
+port = '/dev/ttyACM1'
+
+# Establish a connection with the Arduino
+board = Arduino(port)
+print("Communication Successfully started")
+
+# Set up the pin modes
+led_pin = board.get_pin('d:9:o')
+
+# Blink the LED
+while True:
+    led_pin.write(1)  # Turn the LED on
+    time.sleep(1)  # Delay for 1 second
+    print("LED ON...")
+
+    led_pin.write(0)  # Turn the LED off
+    time.sleep(1)  # Delay for 1 second
+    print("LED OFF...")
