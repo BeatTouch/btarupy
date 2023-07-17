0f29e6aa32b2b4536eb63f3ebf72003de902d43b
JosephGakah <joegakah@gmail.com>
2023-07-17 14:19:13 +0300

    play a beat when led is turned on

---
+++ beatTouch/play_beat.py
@@ -0,0 +1,8 @@
+# import required module
+from playsound import playsound
+
+# for playing punch.wav file
+
+def punch():
+    playsound('../sounds/punch.mp3')
+    print('playing punch sound')
--- init.py
+++ init.py
@@ -1,5 +1,6 @@
 import time
 from pyfirmata import Arduino
+from playsound import playsound
 
 # Define the port where Arduino is connected
 port = '/dev/ttyACM1'
@@ -11,11 +12,17 @@ print("Communication Successfully started")
 # Set up the pin modes
 led_pin = board.digital[8]
 
+#Play a sound function
+def punch():
+    playsound('sounds/punch.mp3')
+    print('playing punch sound')
+
 # Blink the LED
 while True:
     led_pin.write(1)  # Turn the LED on
     time.sleep(1)  # Delay for 1 second
     print("LED ON...")
+    punch()
 
     led_pin.write(0)  # Turn the LED off
     time.sleep(1)  # Delay for 1 second
