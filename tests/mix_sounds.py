b73e5588dd2f27eb9e330e49226a6b1db48159d9
JosephGakah <joegakah@gmail.com>
2023-07-20 16:09:44 +0300

    introducing time between each

--- tests/mix_sounds.py
+++ tests/mix_sounds.py
@@ -1,11 +1,6 @@
-from pydub import AudioSegment
-from pydub.playback import play
+import time
+from playsound import playsound
 
-audio1 = AudioSegment.from_file("../sounds/tom_m.ogg") #your first audio file
-audio2 = AudioSegment.from_file("chunk2.wav") #your second audio file
 
-mixed = audio1.overlay(audio2)          #combine , superimpose audio files
-mixed1  = mixed.overlay(audio3)          #Further combine , superimpose audio files
-#If you need to save mixed file
-mixed1.export("mixed.wav", format='wav') #export mixed  audio file
-play(mixed1)
+playsound('../sounds/cowbell.ogg')
+playsound('../sounds/hihat_open.ogg')
--- tests/read_piezo_plate.py
+++ tests/read_piezo_plate.py
@@ -40,18 +40,21 @@ piezo_plate_2_input.read()
 time.sleep(0.1)
 
 def plate1Input():
-    if(piezo_plate_1_input.read() > 0.3 ):
+    if(piezo_plate_1_input.read() > 0.2):
         print("Plate 1: " + str(piezo_plate_1_input.read()))
         led_pin1.write(1)
-        ride1()
+        
+        playsound('sounds/tom_m.ogg')
         time.sleep(0.1)
         led_pin1.write(0)
 
+
 def plate2Input():
-    if(piezo_plate_2_input.read() > 0.3):
+    if(piezo_plate_2_input.read() > 0.2):
         print("Plate 2: " + str(piezo_plate_2_input.read()))
         led_pin2.write(1)
-        ride2()
+        
+        playsound('sounds/rim.ogg')
         time.sleep(0.1)
         led_pin2.write(0)
 
@@ -63,3 +66,5 @@ if __name__ == '__main__':
 
         p1.start()
         p.start()
+
+        time.sleep(0.1)
